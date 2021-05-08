44f625649b6dc7ffa4fc8ba6ede5ba5f0b7249cb
Anupama Dissanayake <61502343+Anu2001dev@users.noreply.github.com>
2021-05-08 18:38:51 +0530

    🐛 ommits uncompleted sentences

--- vdex/vdex.py
+++ vdex/vdex.py
@@ -7,6 +7,7 @@ if sys.platform == "linux":
     import subprocess
 
     def say(text: str):
+        print(text)
         cmd = f"nanotts-git -i -v en-US --speed 0.8 --pitch 1.4 -w -p ".split()
         cmd.insert(2, text)
         subprocess.call(
@@ -14,6 +15,7 @@ if sys.platform == "linux":
             stdout=subprocess.DEVNULL,
             stderr=subprocess.DEVNULL,
         )
+        subprocess.Popen("rm nanotts-output-*.wav", shell=True)
 
 
 else:
@@ -27,36 +29,54 @@ else:
         engine.runAndWait()
 
 
+headers = {
+    "User-Agent": "Mozilla/5.0 (Windows NT 10.0; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/87.0.4280.88 Safari/537",
+    "Accept": "text/html,application/xhtml+xml,application/xml;q=0.9,*/*;q=0.8",
+    "Accept-Language": "en-US,en;q=0.5",
+    "Accept-Encoding": "gzip, deflate",
+    "DNT": "1",
+    "Connection": "keep-alive",
+    "Upgrade-Insecure-Requests": "1",
+}
+
+
 async def get_data(query: str) -> str:
     """get data required for processing"""
-    headers = {
-        "User-Agent": "Mozilla/5.0 (Windows NT 10.0; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/87.0.4280.88 Safari/537",
-        "Accept": "text/html,application/xhtml+xml,application/xml;q=0.9,*/*;q=0.8",
-        "Accept-Language": "en-US,en;q=0.5",
-        "Accept-Encoding": "gzip, deflate",
-        "DNT": "1",
-        "Connection": "keep-alive",
-        "Upgrade-Insecure-Requests": "1",
-    }
 
     async with aiohttp.ClientSession(headers=headers) as session:
         async with session.get(f"https://google.com/search?q={query}") as resp:
             return await resp.text()
 
 
+def process_data(data):
+    soup = BeautifulSoup(data, "lxml")
+    answer_selectors = [
+        # ".xpdopen > div:first-child > div:first-child > div:first-child > div:first-child > div:first-child"
+        # ".Z0LcW .XcVN5d .AZCkJd"
+        # ".c2xzTb > div:nth-child(1) > div:nth-child(1) > div:nth-child(2) > div:nth-child(1) > div:nth-child(1) > div:nth-child(1)"
+        # ".c2xzTb > div:nth-child(1) > div:nth-child(1) > \
+        # div:nth-child(2) > div:nth-child(1) > div:nth-child(1) > div:nth-child(1)",
+        ".Z0LcW",
+        "div.wDYxhc:nth-child(2) > div:nth-child(1) > div:nth-child(1)",
+        ".kno-rdesc > span:nth-child(2)",
+        ".g > div > div > div:nth-child(2) > span > span:last-child",
+    ]
+    for selector in answer_selectors:
+        answer: Tag = soup.select_one(selector)
+        if answer and answer.text:
+            return answer.text
+
+
 async def main():
     """The main function of the package that puts everything together"""
-    query = input("What do you want to search today? ")
-    # query = "google"
-    data = await get_data(query)
-    soup = BeautifulSoup(data, "lxml")
-    result: Tag = (
-        soup.select_one(
-            ".c2xzTb > div:nth-child(1) > div:nth-child(1) > \
-        div:nth-child(2) > div:nth-child(1) > div:nth-child(1) > div:nth-child(1)"
-        )
-        or soup.select_one(".kno-rdesc > span:nth-child(2)")
-        or soup.select_one(".g > div > div > div:nth-child(2) > span > span:last-child")
-    )
 
-    say(result.text)
+    # query = "explosive negative pushups"
+    # if "query" not in globals():
+    query = input("What do you want to search today? ")
+    data = await get_data(query)
+    answer = process_data(data)
+    print(answer)
+    if answer.endswith("..."):
+        answer = ". ".join(answer.split(". ")[:-1])
+
+    say(answer)
