d605dc8dc009bc8ded67c53ee7e7308a0c1215ac
Anupama Dissanayake <61502343+Hyperx837@users.noreply.github.com>
2022-07-27 09:59:10 +0530

    updated the whole script and added color

--- vdex/__main__.py
+++ vdex/__main__.py
@@ -1,19 +1,8 @@
 import sys
 
 if __name__ == "__main__":
-    major = sys.version_info[0]
-    minor = sys.version_info[1]
-
-    pyversion = (
-        str(sys.version_info[0])
-        + "."
-        + str(sys.version_info[1])
-        + "."
-        + str(sys.version_info[2])
-    )
-
-    if sys.version_info >= (3, 9):
-        print("Vdex is only tested on Python 3.9+\nYou are using Python {pyversion}")
+    if sys.version_info < (3, 9):
+        print("Vdex is only tested on Python 3.9+\nYou are using Python {}.{}.{}".format(*sys.version_info))
         confirm = input(
             "This can cause unexpected errors. Do you wanna continue? [Y/n] "
         )
@@ -22,5 +11,4 @@ if __name__ == "__main__":
 
     from vdex.vdex import main
 
-    while True:
-        main()
+    main()
--- vdex/vdex.py
+++ vdex/vdex.py
@@ -1,36 +1,26 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
 import sys
+from colorama import Fore
 
+from typing import Optional, Tuple
+from urllib import parse
 import requests
+
 from bs4 import BeautifulSoup, Tag
 
-if sys.platform == "linux":
-    import subprocess
-
-    def say(text: str):
-        print(text)
-        cmd = f"nanotts-git -i -v en-US --speed 0.8 --pitch 1.4 -w -p ".split()
-        cmd.insert(2, text)
-        subprocess.call(
-            cmd,
-            stdout=subprocess.DEVNULL,
-            stderr=subprocess.DEVNULL,
-        )
-        subprocess.Popen("rm nanotts-output-*.wav", shell=True)
+blue = "\033[3;27m"
+grey = '\033[0;248m'
+nocolor="\033[0m"
 
 
-else:
-    import pyttsx3
-
-    engine = pyttsx3.init()
-    engine.setProperty("rate", 125)
-
-    def say(text: str):
-        engine.say(text)
-        engine.runAndWait()
-
+query = " ".join(sys.argv[1:])
 
 headers = {
-    "User-Agent": "Mozilla/5.0 (Windows NT 10.0; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/87.0.4280.88 Safari/537",
+    "User-Agent": (
+        "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 "
+        "(KHTML, like Gecko) Chrome/91.0.4472.114 Safari/537.36"
+    ),
     "Accept": "text/html,application/xhtml+xml,application/xml;q=0.9,*/*;q=0.8",
     "Accept-Language": "en-US,en;q=0.5",
     "Accept-Encoding": "gzip, deflate",
@@ -39,29 +29,59 @@ headers = {
     "Upgrade-Insecure-Requests": "1",
 }
 
+answer_selectors = {
+        "div.wDYxhc:nth-child(2) > div.IZ6rdc": "",  # main paragraph
+        ".Z0LcW": "",  # highlted result
+        ".XDTKBd": "",  # main result
+        ".hgKElc": ".yuRUbf > a",  # featured snippet
+        ".kno-rdesc > span:nth-child(1)": "",
+        ".kno-rdesc > span:nth-child(2)": "",  # sidebar result
+        ".VwiC3b": ".yuRUbf > a",  # first result
+}
 
-def process_data(data):
-    soup = BeautifulSoup(data, "lxml")
-    answer_selectors = [
-        ".Z0LcW",
-        "div.wDYxhc:nth-child(2) > div:nth-child(1) > div:nth-child(1)",
-        ".kno-rdesc > span:nth-child(2)",
-        ".g > div > div > div:nth-child(2) > span > span:last-child",
-    ]
-    for selector in answer_selectors:
-        answer: Tag = soup.select_one(selector)
+def process_answer(answer: str) -> str:
+    """remove last sentence if it's incomplete"""
+    if not answer.endswith("..."):
+        return answer
+
+    # 8 characters doesn't make a meaningful sentence
+    sentences = answer.split(". ")
+    if len(sentences[-1]) < 8:
+        sentences = sentences[:-1]
+
+    return ". ".join(sentences)
+
+
+def get_answer(soup) -> Tuple[str, str]:
+    """get google results"""
+    for ans_selector, link_selector in answer_selectors.items():
+        answer: Tag = soup.select_one(ans_selector)
         if answer and answer.text:
-            return answer.text
+            link: str = (
+                soup.select_one(link_selector)["href"]
+                if link_selector
+                else ""
+            )
+            answer = process_answer(answer.text)
+            return answer, link
+
+    return answer, ""
+
+
+
+def handle_query(query):
+    escaped_query = parse.quote(query)
+    url = f"https://google.com/search?q={escaped_query}"
+    markup = requests.get(url, headers=headers).text
+    soup = BeautifulSoup(markup, "lxml")
+    return get_answer(soup)
 
 
 def main():
-    """The main function of the package that puts everything together"""
+    answer, link = handle_query(query)
+    print(Fore.LIGHTCYAN_EX + link)
+    print(Fore.LIGHTYELLOW_EX + answer)
+    print(Fore.WHITE)
 
-    query = input("What do you want to search today? ")
-    data = requests.get(f"https://google.com/search?q={query}", headers=headers).text
-    answer = process_data(data)
-    print(answer)
-    if answer.endswith("..."):  # removes the last sentense if it's incompleted
-        answer = ". ".join(answer.split(". ")[:-1])
-
-    say(answer)
+if __name__ == "__main__":
+    main()
